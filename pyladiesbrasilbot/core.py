a5b1371d983cf7928ace24533db4771f8eb94a1b
naanadr <fsandrade25@gmail.com>
2020-10-19 19:49:23 -0300

    Atualiza comandos do bot
    Adiciona boas vindas aos novos membros e remove comandos criados na fase de teste

--- pyladiesbrasilbot/core.py
+++ pyladiesbrasilbot/core.py
@@ -1,21 +1,38 @@
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
-from conf.settings import BASE_API_URL, TELEGRAM_TOKEN
+from conf.settings import TELEGRAM_TOKEN
 
 
 def start(update, context):
-    response_message = "=^._.^="
+    response_message = (
+        "Olá, eu sou a PyLadies Brasil Bot! "
+        "Ainda estou em construção, mas espero poder te ajudar."
+    )
     context.bot.send_message(chat_id=update.effective_chat.id, text=response_message)
 
 
-def http_cats(update, context):
-    context.bot.sendPhoto(
-        chat_id=update.effective_chat.id, photo=BASE_API_URL + context.args[0]
-    )
+def help(update, context):
+    message = "Lista de comandos ainda em construção..."
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+
+def welcome(update, context):
+    for new_member in update.message.new_chat_members:
+        if not new_member.is_bot:
+            message = (
+                f"Seja bem vinda, {new_member.first_name} "
+                f"{new_member.last_name} "
+                f"(@{new_member.username}) ao {update.message.chat.title}!"
+            )
+            context.bot.sendPhoto(
+                chat_id=update.effective_chat.id,
+                photo=open("pyladiesbrasilbot/utils/welcome_pyladies_recife.jpg", "rb"),
+                caption=message,
+            )
 
 
 def unknown(update, context):
-    response_message = "Meow? =^._.^="
+    response_message = "Comando inválido! Insira /help para ver a lista de comandos"
     context.bot.send_message(chat_id=update.effective_chat.id, text=response_message)
 
 
@@ -25,8 +42,11 @@ def main():
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("http", http_cats, pass_args=True))
-    dispatcher.add_handler(MessageHandler(Filters.all, unknown))
+    dispatcher.add_handler(CommandHandler("help", help))
+    dispatcher.add_handler(
+        MessageHandler(Filters.status_update.new_chat_members, welcome)
+    )
+    dispatcher.add_handler(MessageHandler(Filters.command, unknown))
 
     updater.start_polling()
 
