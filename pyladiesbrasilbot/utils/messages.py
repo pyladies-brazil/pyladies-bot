9d4bd881adc220b43791adb6a8fff7b72f4153b0
naanadr <fsandrade25@gmail.com>
2020-10-21 17:29:45 -0300

    Adiciona um arquivo com mensagens
    Mensagens que serão utilizadas pelo bot

---
+++ pyladiesbrasilbot/utils/messages.py
@@ -0,0 +1,8 @@
+START_MESSAGE = (
+    "Olá, eu sou a PyLadies Brasil Bot! "
+    "Ainda estou em construção, mas espero poder te ajudar."
+)
+
+WELCOME_NEW_MEMBER_MESSAGE = (
+    "Seja bem vinda, {first_name} {las_name} (@{username}) ao {chat_title}!"
+)
